880d267c85adb9259a287cd5bbbb37337c7d88b7
Yurik Hayrapetyan <yurik.hayrapetyan@cyberhull.com>
2025-04-18 16:11:57 +0400

    Add email sender "Mailpit"

--- requirements/fpm/bin/start.sh
+++ requirements/fpm/bin/start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+: "${SERVER_NAME:?Missing SERVER_NAME}"
+: "${MAILPIT_HOST:?Missing MAILPIT_HOST}"
+: "${MAILPIT_PORT:?Missing MAILPIT_PORT}"
+
+[ ! -z "${SERVER_NAME}" ]   && sed -i "s|SERVER_NAME|${SERVER_NAME}|" /etc/msmtprc/msmtprc
+[ ! -z "${MAILPIT_HOST}" ]   && sed -i "s|MAILPIT_HOST|${MAILPIT_HOST}|" /etc/msmtprc/msmtprc
+[ ! -z "${MAILPIT_PORT}" ]   && sed -i "s|MAILPIT_PORT|${MAILPIT_PORT}|" /etc/msmtprc/msmtprc
+
 # All of the provided changes already applied to php.ini from config
 # sed -i "s/memory_limit = .*/memory_limit = 1G/" /etc/php/8.1/cli/php.ini
 # sed -i "s/upload_max_filesize = .*/upload_max_filesize = 128M/" /etc/php/8.1/cli/php.ini
